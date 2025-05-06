912cc7d589e397fa52987ce7db417ce0db0a4ec7
MK <101432939+Mubangakampamba@users.noreply.github.com>
2025-05-06 03:06:05 -0500

    Update mySQL_usa_online_shopping.sql

--- mySQL_usa_online_shopping.sql
+++ mySQL_usa_online_shopping.sql
@@ -1,13 +1,13 @@
--- create a new schema, then import the data into mySQL
--- observe the distinct industries in the data, ordered in ascending order
--- after observing the output, export this recordset, open it in excel and save it as its own table
+-- make a schema, then import 'usa_online_shopping' into mySQL
+-- check the distinct industries in the table, ordered in ascending order
+-- after checking the output, export the recordset, open it in excel and save it as its own table
 
 select distinct Industry
 from usa_online_shopping_raw
 order by Industry;
 
 -- this recordset can be used to observe the companies which operate within distinct industries
--- for example, companies in the Retail industry can be shown by
+-- for example, companies in the Retail industry can be seen by:
 
 select *
 from usa_online_shopping_raw
